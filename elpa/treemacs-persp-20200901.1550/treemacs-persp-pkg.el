f0ce08206d4c21181fa0d76243ca0e19506a2f95
Jared Nelsen <jnelsen788@gmail.com>
2021-01-09 13:51:22 -0500

    Renamed gitignore and added elpa

---
+++ elpa/treemacs-persp-20200901.1550/treemacs-persp-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "treemacs-persp" "20200901.1550" "Persp-mode integration for treemacs" '((emacs "25.2") (treemacs "0.0") (persp-mode "2.9.7") (dash "2.11.0")) :commit "6b045fd585421ab3c9e1185c2508d34af700490b" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
